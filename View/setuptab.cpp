33e86ceb15292af94a687c76a911c44cae48729c
xopherira <xopherira@msn.com>
2016-08-16 09:55:11 -0400

    Moved code to Setup tab

--- View/setuptab.cpp
+++ View/setuptab.cpp
@@ -5,6 +5,14 @@
 #include "ui_mainview.h"
 #include "View/setuptab.h"
 #include "View/addlineitem.h"
+#include "View/searchdialog.h"
+#include "View/missingdatadialog.h"
+#include "View/overcostlimitdialog.h"
+#include "View/ordersubmitteddialog.h"
+#include <string>
+#include <sstream>
+#include <QDir>
+#include <QFile>
 #include <QTableWidget>
 #include <QString>
 #include <QDir>
@@ -29,8 +37,9 @@
 #include <QUrlQuery>
 
 static auto RESOURCE_PREFIX = QStringLiteral(":/xml");
-
+static auto OUTFILE_PREFIX = QStringLiteral("C:/");
 static QString FILEERROR_MSG = QStringLiteral("ERROR OPENING FILE");
+
 QString defaultXML;
 
 /************************************************************************
@@ -281,7 +290,7 @@ void SetupTab::XmlAddressSearchRequestParsing(QXmlStreamReader &XmlFileReader)
             ui->leShipToAddress_Name->setText(dname); // = dname;
             ui->leShipToAddress_Address1->setText(address1); //= address1;
             ui->leShipToAddress_Address2->setText(address2); //= address2;
-            ui->leShipToAddress_Address3->setText(address3); //= address3;
+            //ui->leShipToAddress_Address3->setText(address3); //= address3;
             ui->leShipToAddress_City->setText(city); //= city;
             ui->leShipToAddress_State->setText(state); //= state;
             ui->leShipToAddress_Zip->setText(zip); //= zip;
@@ -308,6 +317,200 @@ void SetupTab::on_leShipToDealer_ID_editingFinished()
 {
     GetAddressXML();
 }
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+void SetupTab::AddLineItemFromDialog(QString & item)
+{
+    ui->tblOrderLinesWidget->setItem(0,0, new QTableWidgetItem(item));
+}
+
+void SetupTab::on_btnAddLineItem_GetMacPacPart_clicked()
+{
+    SearchDialog *dialog = new SearchDialog;
+
+    if(dialog->exec())
+    {
+        QMap<QString, QString> m = dialog->getMap();
+
+        QString partName = m.value("PartName");
+        ui->leAddLineItem_PartNumber->setText(partName);
+
+        QString partDesc = m.value("PartDesc");
+        ui->leAddlineItem_Description->setText(partDesc);
+
+        QString partCost = m.value("PartCost");
+        ui->leAddLineItem_Value->setText(partCost);
+
+        //Default the quantity to 1 for users
+        ui->leAddLineItem_Quantity->setText("1");
+    }
+}
+
+void SetupTab::updateExtendedCostTotal() {
+    int rowCt = ui->tblOrderLinesWidget->rowCount();
+    if (rowCt == 0) {
+        ui->leOrderTotal->setText("0.00");
+        return;
+    }
+    QString uiTotal;
+    float tmpTotal;
+    QTableWidgetItem *wi;
+    for (int i = 0; i < rowCt; i++) {
+        wi = ui->tblOrderLinesWidget->item(i,4);
+        tmpTotal += wi->text().toFloat();
+        if(tmpTotal > 100)
+        {
+            this->ui->tblOrderLinesWidget->removeRow(i);
+            OverLimitDialog();
+            return;
+        }
+    }
+    uiTotal.setNum(tmpTotal);
+    ui->leOrderTotal->setText(uiTotal);
+}
+
+void SetupTab::resetFields() {
+    ui->leAddLineItem_PartNumber->setText("");
+    ui->leAddlineItem_Description->setText("");
+    ui->leAddLineItem_Value->setText("");
+    ui->leAddLineItem_Quantity->setText("");
+}
+
+
+void SetupTab::on_btnAddLineItem_AddLine_clicked()
+{
+    QString partName;
+    QString partDesc;
+    QString partCost;
+    QString partQty;
+    QString extCost;
+    float xCost;
+
+    partName = ui->leAddLineItem_PartNumber->text();
+    partDesc = ui->leAddlineItem_Description->text();
+    partCost = ui->leAddLineItem_Value->text();
+    partQty = ui->leAddLineItem_Quantity->text();
+    //START: Extended Cost Mod - FJD - 8.9.2016
+    xCost = partQty.toFloat() * partCost.toFloat();
+    extCost.setNum(xCost);
+    //END: Extended Cost Mod
+
+    if(xCost > 100)
+    {
+        OverLimitDialog();
+        return;
+    }
+
+    if((PartOkToAdd(partName, partDesc, partCost, partQty) == true) && (xCost < 100))
+    {
+        int row = ui->tblOrderLinesWidget->rowCount();
+        ui->tblOrderLinesWidget->insertRow(row);
+        ui->tblOrderLinesWidget->setItem(row,0, new QTableWidgetItem (partName));
+        ui->tblOrderLinesWidget->setItem(row,1, new QTableWidgetItem (partDesc));
+        ui->tblOrderLinesWidget->setItem(row,2, new QTableWidgetItem (partCost));
+        ui->tblOrderLinesWidget->setItem(row,3, new QTableWidgetItem (partQty));
+        ui->tblOrderLinesWidget->setItem(row,4, new QTableWidgetItem (extCost));
+        updateExtendedCostTotal();
+        resetFields();
+    }
+}
+
+bool SetupTab::PartOkToAdd(QString partName, QString partDesc, QString partCost, QString partQty) {
+    bool retval = true;
+    if (partName == "") {
+        retval = false;
+    }
+    if (partDesc == "") {
+        retval = false;
+    }
+    if (partCost == "") {
+        retval = false;
+    }
+    if (partQty == "" || partQty == "0" || partQty.contains("-")) {
+        retval = false;
+    }
+    if (retval == false) {
+            MissingDataDialog *mdDialog = new MissingDataDialog;
+            mdDialog->show();
+    }
+    return retval;
+}
+
+void SetupTab::handleDeleteSelectedRow()
+{
+    QList<QTableWidgetItem*> selectionRangeList = this->ui->tblOrderLinesWidget->selectedItems();
+    int rowIndex;
+    QListIterator<QTableWidgetItem*> selectionRangeListIter(selectionRangeList);
+    while(selectionRangeListIter.hasNext()){
+        rowIndex = ((int)((QTableWidgetItem*)selectionRangeListIter.next())->row());
+        this->ui->tblOrderLinesWidget->removeRow(rowIndex);
+    }
+}
+
+void SetupTab::on_btnRecapAndSubmit_Clear_clicked()
+{
+    handleDeleteSelectedRow();
+    updateExtendedCostTotal();
+}
+
+void SetupTab::OverLimitDialog()
+{
+    overCostLimitDialog *dialog = new overCostLimitDialog;
+    dialog->exec();
+}
+
+void SetupTab::on_btnSubmitOrder_clicked()
+{
+
+//  /*  //QString testPO;
+//    Ui_SetupTab * test = new Ui_SetupTab;
+//    test->leShipToDealer_PO->text();
+//     //qDebug() << "TEST THE POOOOOOOOOOOOOOOO" << testPO;
+//    //OrderSubmittedDialog *dialog = new OrderSubmittedDialog;
+//   //WriteXml(*/);
+//   // dialog->exec();
+}
+
+//void AddLineItem::ProcessSetUpTab(SetupTab * a)
+//{
+
+//}
+/************************************************************************
+ *
+ *  WriteXml()
+ *
+ ************************************************************************/
+void SetupTab::WriteXml()
+{
+
+
+
+    QDir res_dir (OUTFILE_PREFIX);
+    auto path = res_dir.filePath("XmlForMacPac.xml");
+    QFile res_file(path);
+
+  //  basefilename = QFileDialog::getSaveFileName(this,tr("Save Xml"), ".",tr("Xml files (*.xml)"));
+
+    if(!res_file.open(QIODevice::WriteOnly))
+    {
+        //TODO Send Error message
+        qDebug() << FILEERROR_MSG;
+    }
+    res_file.open(QIODevice::WriteOnly);
+    QXmlStreamWriter xmlWriter(&res_file);
+    xmlWriter.setAutoFormatting(true);
+    xmlWriter.writeStartDocument();
+    xmlWriter.writeStartElement("Communication");//**********************************************
+    xmlWriter.writeTextElement("CommunicationType", "SPO");
+    xmlWriter.writeStartElement("Contexts");//***********************************************
+    xmlWriter.writeTextElement("ParserVersion", "1.0");
+    xmlWriter.writeTextElement("POGUID", "SPO12345");
+    xmlWriter.writeEndElement();//End Contexts*********************************************
+    xmlWriter.writeEndElement();//End Communication********************************************
+   // ShowXmlOnScreen();
+}
+
+
 
 
 
--- View/setuptab.h
+++ View/setuptab.h
@@ -15,10 +15,15 @@ class SetupTab : public QWidget, public Ui::SetupTab
     Q_OBJECT
 
 public:
-Ui::SetupTab *ui;
+
     explicit SetupTab(QWidget *parent = 0);
 
     ~SetupTab();
+void AddLineItemFromDialog(QString &item);
+bool PartOkToAdd(QString partName, QString partDesc, QString partCost, QString partQty);
+void handleDeleteSelectedRow();
+void updateExtendedCostTotal();
+void OverLimitDialog();
     void GetAddressXML();
     void XmlAddressSearchRequestParsing(QXmlStreamReader &XmlFileReader);
     QString GetDefaultXML();
@@ -28,6 +33,11 @@ public slots:
 private slots:
     //void on_btnAddShipToAddress_clicked();
     void on_leShipToDealer_ID_editingFinished();
+    void on_btnAddLineItem_GetMacPacPart_clicked();
+    void on_btnAddLineItem_AddLine_clicked();
+    void on_btnRecapAndSubmit_Clear_clicked();
+
+    void on_btnSubmitOrder_clicked();
 private:
     QString m_basefilename;
     QString basefilename;
@@ -36,6 +46,12 @@ private:
     QString ReadBaseXMLFromInternalResource();
     QString ReadBaseXMLFile();
     void XmlSpoParsing(QXmlStreamReader &XmlFileReader);
+    bool overCostLimit;
+    Ui::SetupTab *ui;
+    bool AddLineItem_OrderTotal(QString orderTotal);
+    void resetFields();
+    bool GetOrderTotal();
+    void WriteXml();
 };
 
 #endif // SETUPTAB_H
